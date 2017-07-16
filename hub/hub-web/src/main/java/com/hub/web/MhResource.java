dc2d5ace6ba22917e5583bbad3d6b0f824a427e1
iLakeYC <mail@ilakeyc.gift>
2017-07-16 18:06:09 +0800

    N/A

--- hub/hub-web/src/main/java/com/hub/web/MhResource.java
+++ hub/hub-web/src/main/java/com/hub/web/MhResource.java
@@ -1,5 +1,6 @@
 package com.hub.web;
 
+import com.hub.model.Person;
 import com.sun.jersey.api.view.Viewable;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
@@ -10,6 +11,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author ilakeyc
@@ -23,6 +26,49 @@ public class MhResource extends BaseResource {
     @GET
     @Produces(MediaType.TEXT_HTML)
     public Response index() {
+
+        Person p1 = new Person();
+        p1.setName("马辉");
+        p1.setAge(20);
+        p1.setGender(1);
+
+        Person p2 = new Person();
+        p2.setName("理性南");
+        p2.setAge(22);
+        p2.setGender(2);
+
+        Person p3 = new Person();
+        p3.setName("郭众先");
+        p3.setAge(21);
+        p3.setGender(1);
+
+        Person p4 = new Person();
+        p4.setName("刘晒");
+        p4.setAge(24);
+        p4.setGender(1);
+
+        Person p5 = new Person();
+        p5.setName("夏一尘");
+        p5.setAge(22);
+        p5.setGender(1);
+
+        List<String> p5_friends = new ArrayList<String>();
+        p5_friends.add(p1.getName());
+        p5_friends.add(p2.getName());
+        p5_friends.add(p3.getName());
+        p5_friends.add(p4.getName());
+        p1.setFriends(p5_friends.toString());
+
+        List<Person> personList = new ArrayList<Person>();
+
+        personList.add(p1);
+        personList.add(p2);
+        personList.add(p3);
+        personList.add(p4);
+        personList.add(p5);
+
+        request.setAttribute("persons", personList);
+
         return Response.ok(new Viewable("index")).build();
     }
 
--- hub/hub-web/src/main/webapp/WEB-INF/pages/com/hub/web/MhResource/index.jsp
+++ hub/hub-web/src/main/webapp/WEB-INF/pages/com/hub/web/MhResource/index.jsp
@@ -8,4 +8,51 @@
 <%@ page language="java" pageEncoding="UTF-8" %>
 <%@ include file="/WEB-INF/commons/taglibs.jsp" %>
 
-<h1>马辉你好</h1>
+<%-- 显示所有的人 --%>
+
+<div id="foo" style="margin-left: 30px">
+
+    <h1>人物列表</h1>
+
+    <%--${}--%>
+    <%--${persons[0].name}--%>
+
+    <c:forEach var="person" items="${persons}">
+        ${person.name}
+        <ul>
+            <li>年龄：${person.age}</li>
+            <li>性别：
+                <c:choose>
+                    <c:when test="${person.gender == 1}">
+                        <c:choose>
+                            <c:when test="${person.status == 0}">男</c:when>
+                            <c:otherwise>不可用</c:otherwise>
+                        </c:choose>
+                    </c:when>
+                    <c:when test="${person.gender == 2}">女</c:when>
+                    <c:otherwise>N/A</c:otherwise>
+                </c:choose>
+            </li>
+            <li>状态：
+                <c:if test="${person.status == 0}">正常</c:if>
+                <c:if test="${person.status != 0}">不可用</c:if>
+            </li>
+            <li>操作：<a class="removeButton btn-danger btn" data-name="${person.name}" data-id="${person.id}">删除</a></li>
+        </ul>
+    </c:forEach>
+
+</div>
+
+<script>
+
+    $(function () {
+
+        $('.removeButton').click(function () {
+            var id = $(this).attr('data-id');
+            var name = $(this).attr('data-name');
+            console.log(name, id);
+        })
+
+    })
+
+</script>
